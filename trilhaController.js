8eb7181ce98746aaeb1a871483d735d5c1d8b967
Naira Magalhães <52166063+urielleverkhan@users.noreply.github.com>
2021-09-01 17:30:49 -0300

    atchim pronto

--- trilhaController.js
+++ trilhaController.js
@@ -5,24 +5,46 @@
     myApp.controller('TrilhaController', function($scope){
         $scope.teste = 'OLA MUNDÃO';
         $scope.count = 0;
+    
         const modal = document.querySelector(".modal-content");
         const conteudo = document.querySelector(".prosseguir");
         const botaoProsseguir = document.querySelector(".botao-prosseguir");
+        const imgParado = document.querySelector(".imgParado");
+        const imgQuase = document.querySelector(".imgQuase");
+        const atchim = document.querySelector(".atchim")
+        const imgParadoAtchim = document.querySelector(".imgParadoAtchim")
 
-        $scope.abrir = function(){
+        $scope.atchim = function(){
             modal.style.display = "flex";
             botaoProsseguir.style.display = "block";
             conteudo.style.display = "none";
+            pararAtchim();
         }
 
         $scope.fechar = function(){
             modal.style.display = "none";
+            imgParadoAtchim.style.display="none"; 
+            atchim.style.display = "flex";
         }
 
         $scope.prosseguir = function(){
             conteudo.style.display = "flex";
             botaoProsseguir.style.display = "none";
         }
-        
+        $scope.cocegas = function(){
+            imgParado.style.display = "none";
+            imgQuase.style.display = "flex";
+        }
+        $scope.semCocegas = function(){
+            imgParado.style.display = "flex";
+            imgQuase.style.display = "none";
+        }
+
+        function pararAtchim(){
+            setTimeout(function(){ 
+                imgParadoAtchim.style.display="flex"; 
+                atchim.style.display = "none";
+             }, 600);
+        }
     });
 })();
